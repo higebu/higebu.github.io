90ddb054fb3f83463ac7aed5ce76ff4e95446704
Yuya Kusakabe <yuya.kusakabe@gmail.com>
2016-04-27 15:34:40 +0900

    Add new.sh

---
+++ new.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+TITLE=$1
+SLUG=$(date "+%Y-%m-%d")-$2
+DATE=$(date "+%Y-%m-%d %H:%M")
+CATEGORY=$3
+TAGS=$4
+SUMMARY=$5
+
+BODY="title: $TITLE
+Slug: $SLUG
+Date: $DATE
+Category: $CATEGORY
+Tags: $TAGS
+Summary: $SUMMARY
+
+## Image Tag
+"
+
+echo "${BODY}" > content/${SLUG}.md
+echo '{% img [class name] /images/image-file-name [width [height]] [title text] %}' >> content/${SLUG}.md
+$EDITOR "content/${SLUG}.md"
